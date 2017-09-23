bf859498dc993f17cc10531c1b3dc479f6d1c722
mindonly <thesanch@gmail.com>
2017-09-23 19:23:39 -0400

    north, west, northwest functions added.

--- align.cpp
+++ align.cpp
@@ -29,19 +29,48 @@ void printSeq(const std::vector<char> &seq) {
     cout << endl;
 }
 
-/* int gapWest() {
+int gapNorth(matrix<int> &mat, int row, int col) { 
+    if (row == 0 || col == 0) {
+        cerr << "\nerror: nucleotide coordinates cannot be zero.\n";
+        exit(-1);
+    }
 
+    cout << "\ngapNorth():\n";
+    cout << "(" << row << ", " << col << ")\n";
+    // North-adjusted row
+    int adjRow = row - 1;
+    cout << "(" << adjRow << ", " << col << ")\n";
+
+    cout << mat(adjRow, col) - 2;
+    return mat(adjRow, col) - 2;
 }
 
-int gapNorth() {
+int gapWest(matrix<int> &mat, int row, int col) {
+    if (row == 0 || col == 0) {
+        cerr << "\nerror: nucleotide coordinates cannot be zero.\n";
+        exit(-1); 
+    }
 
-} */
+    cout << "\ngapWest():\n";
+    cout << "(" << row << ", " << col << ")\n";
+    // West-adjusted col
+    int adjCol = col - 1;
+    cout << "(" << row << ", " << adjCol << ")\n";
+
+    cout << mat(row, adjCol) - 2;
+    return mat(row, adjCol) - 2;
+}
 
 int match(std::vector<char> &s, 
-           std::vector<char> &t, 
-           int row, int col) {
+          std::vector<char> &t, 
+          int row, int col) {
     
-    cout << s[row-1] << " " << t[col-1] << endl;
+    if (row == 0 || col == 0) {
+        cerr << "\nerror: nucleotide coordinates cannot be zero.\n";
+        exit(-1); 
+    }
+            
+    // cout << s[row-1] << " " << t[col-1] << endl;
     
     if (s[row-1] == t[col-1])
         return 1;
@@ -49,9 +78,25 @@ int match(std::vector<char> &s,
         return -1;
 }
 
-/* int northWest() {
+int northWest(matrix<int> &mat,
+              std::vector<char> &s,
+              std::vector<char> &t,
+              int row, int col) {
 
-} */
+    if (row == 0 || col == 0) {
+        cerr << "\nerror: nucleotide coordinates cannot be zero.\n";
+        exit(-1); 
+    }
+
+    cout << "\nnorthWest():\n";
+    cout << "(" << row << ", " << col << ")\n";
+    int adjRow = row - 1;
+    int adjCol = col - 1;
+    cout << "(" << adjRow << ", " << adjCol << ")\n";
+
+    cout << mat(adjRow, adjCol) + match(s, t, row, col);
+    return mat(adjRow, adjCol) + match(s, t, row, col);
+}
 
 /* void computePos() {
 
@@ -80,4 +125,12 @@ int main(int argc, char* argv[]) {
     // create and zero-out similarity matrix
     matrix<int> sim_mat(s.size(), t.size());
     sim_mat.clear();
+
+    for (int i = 1; i <= 2; i++) 
+        for (int j = 1; j <= 2; j++) {
+            gapNorth(sim_mat, i, j);
+            gapWest(sim_mat, i, j);
+            northWest(sim_mat, s, t, i, j);
+            cout << endl;
+        }
 }
